e5ac4a3103546ebbb7a0bf91c3005ed61bfc7464
ivannofick <ivannofick@gmail.com>
2022-06-07 23:36:09 +0700

    nyicil tampilan

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,84 +3,56 @@ import React from "react";
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg bg-light">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="#">
-                    Navbar
-                </a>
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">
-                                Home
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">
-                                Link
-                            </a>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <a
-                                className="nav-link dropdown-toggle"
-                                href="#"
-                                id="navbarDropdown"
-                                role="button"
-                                data-bs-toggle="dropdown"
-                                aria-expanded="false"
-                            >
-                                Dropdown
-                            </a>
-                            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li>
-                                    <a className="dropdown-item" href="#">
-                                        Action
-                                    </a>
-                                </li>
-                                <li>
-                                    <a className="dropdown-item" href="#">
-                                        Another action
-                                    </a>
-                                </li>
-                                <li>
-                                    <hr className="dropdown-divider" />
-                                </li>
-                                <li>
-                                    <a className="dropdown-item" href="#">
-                                        Something else here
-                                    </a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link disabled">Disabled</a>
-                        </li>
-                    </ul>
-                    <form className="d-flex" role="search">
-                        <input
-                            className="form-control me-2"
-                            type="search"
-                            placeholder="Search"
-                            aria-label="Search"
-                        />
-                        <button className="btn btn-outline-success" type="submit">
-                            Search
+        <>
+            {/* Navigation*/}
+            <nav
+                className="navbar navbar-expand-lg navbar-light fixed-top shadow-sm"
+                id="mainNav"
+            >
+                <div className="container px-5">
+                    <a className="navbar-brand fw-bold" href="#page-top">
+                        Start Bootstrap
+                    </a>
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarResponsive"
+                        aria-controls="navbarResponsive"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        Menu
+                        <i className="bi-list" />
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarResponsive">
+                        <ul className="navbar-nav ms-auto me-4 my-3 my-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link me-lg-3" href="#features">
+                                    Features
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link me-lg-3" href="#download">
+                                    Download
+                                </a>
+                            </li>
+                        </ul>
+                        <button
+                            className="btn btn-primary rounded-pill px-3 mb-2 mb-lg-0"
+                            data-bs-toggle="modal"
+                            data-bs-target="#feedbackModal"
+                        >
+                            <span className="d-flex align-items-center">
+                                <i className="bi-chat-text-fill me-2" />
+                                <span className="small">Send Feedback</span>
+                            </span>
                         </button>
-                    </form>
+                    </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
+        </>
+
     );
 };
 
